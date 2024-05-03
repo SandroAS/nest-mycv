4a062a6223aecf4500b002d0b87263adbb86da38
Sandro Souza <sandro@greenn.com.br>
2024-05-02 21:15:59 -0300

    pelo amor de deus

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,19 +2,25 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { BadRequestException } from '@nestjs/common';
-
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
   
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
-
   beforeEach(async () => {
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter(user => user.email === email)
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = { id: (Math.random() * 9999999), email, password } as User
+        users.push(user);
+        return Promise.resolve(user);
+      }
+    };
 
     const module = await Test.createTestingModule({
       providers: [
@@ -45,8 +51,24 @@ describe('AuthService', () => {
   it('throws an error if user signs up with email that is in use', async () => {
     fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
-      BadRequestException
+      BadRequestException,
     );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
+  it('throws if an invalid password is provided', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
+      ]);
+    await expect(
+      service.signin('laskdjf@alskdfj.com', 'passowrd'),
+    ).rejects.toThrow(BadRequestException);
   });
 });
 
