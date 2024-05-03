4d433e82928bd6b67aab5f458f06d6e9c9c337dd
Sandro Souza <sandro@greenn.com.br>
2024-05-03 20:56:06 -0300

    ajuste testes users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, NotFoundException, Session, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -16,11 +16,6 @@ export class UsersController {
     private usersService: UsersService,
     private authService: AuthService
   ) {}
-
-  // @Get('/whoami')
-  // whoAmI(@Session() session: any) {
-  //   return this.usersService.findOne(session.userId);
-  // }
 
   @Get('/whoami')
   @UseGuards(AuthGuard)
@@ -50,14 +45,14 @@ export class UsersController {
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
-    if(!user) {
-      return new NotFoundException('user not found');
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
     return user;
   }
 
   @Get()
-  findAllUsers(@Query('id') email: string) {
+  findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
