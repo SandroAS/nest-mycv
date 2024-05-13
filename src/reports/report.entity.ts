af30664d01f7426a072bd4462cca43fb01ee4c04
Sandro Souza <sandro@greenn.com.br>
2024-05-13 20:31:44 -0300

    add columns report

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -8,4 +8,18 @@ export class Report {
   @Column()
   price: string;
 
+  @Column()
+  make: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }
