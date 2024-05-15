907ca5c9b1904ab011896c3940046d09eb5687da
Sandro Souza <sandro@greenn.com.br>
2024-05-14 21:12:04 -0300

    approveReport

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -14,4 +14,10 @@ export class ReportsService {
     report.user = user;
     return this.repo.save(report);
   }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+
+    
+  }
 }
